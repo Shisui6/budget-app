c59bbb0ba10b0f16af50b486e661ffd14a29d8fd
Shisui <37116322+Shisui6@users.noreply.github.com>
2023-03-20 16:17:52 +0100

    Fix linter errors

--- spec/requests/group_activities_spec.rb
+++ spec/requests/group_activities_spec.rb
@@ -76,7 +76,6 @@ RSpec.describe '/group_activities', type: :request do
         end.to change(GroupActivity, :count).by(0)
       end
 
-
       it "renders a response with 422 status (i.e. to display the 'new' template)" do
         post group_activities_url, params: { group_activity: invalid_attributes }
         expect(response).to have_http_status(:unprocessable_entity)
